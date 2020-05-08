b14340eb0c1f38c174f216ada730eae10d4ed087
pikuch <55052790+pikuch@users.noreply.github.com>
2020-05-08 14:26:30 +0200

    added the rendering loop + fps counter

--- clock/js/clock.js
+++ clock/js/clock.js
@@ -3,6 +3,8 @@
 var elem = document.documentElement;
 var btn = document.getElementById("FullscreenButton");
 var canv = document.getElementById("ClockCanvas");
+var ctx = canv.getContext("2d");
+
 
 /* View in fullscreen */
 function openFullscreen() {
@@ -17,6 +19,7 @@ function openFullscreen() {
   } else if (elem.msRequestFullscreen) { /* IE/Edge */
     elem.msRequestFullscreen();
   }
+
 }
 
 /* Close fullscreen */
@@ -36,10 +39,51 @@ window.onresize = function() {
   canv.width = window.innerWidth;
   canv.height = window.innerHeight;
   
-  ctx = canv.getContext("2d");
   for (let i=1; i<50; i++) {
 	ctx.beginPath();
 	ctx.arc(canv.width/2, canv.height/2, canv.height/2/i, 0, 2 * Math.PI);
 	ctx.stroke();
     }
 }
+
+function update(dt) {
+}
+
+function getRndColor() {
+    var r = 255*Math.random()|0,
+        g = 255*Math.random()|0,
+        b = 255*Math.random()|0;
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
+function render() {
+	for (let x=0; x<canv.width/42; x++)
+		for (let y=0; y<canv.height/42; y++) {
+			ctx.fillStyle = getRndColor();
+			ctx.fillRect(x*42, y*42, 42, 42);
+		}	
+}
+
+function renderFPS(dt) {
+	ctx.fillStyle = "white";
+	ctx.fillRect(0, 0, 80, 40);
+	ctx.font = "20px Arial";
+	ctx.fillStyle = "black";
+	ctx.fillText((1/dt).toFixed() + "fps", 10, 30);
+}
+
+
+var lastTime;
+function animate() {
+	var now = Date.now();
+    var dt = (now - lastTime) / 1000.0;
+
+    update(dt);
+    render();
+	renderFPS(dt);
+
+    lastTime = now;
+	requestAnimationFrame(animate);
+}
+
+animate()
