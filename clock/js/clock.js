1c9b29cd198a43d285231bb77b4453e0658e9fbd
pikuch <55052790+pikuch@users.noreply.github.com>
2020-05-11 13:11:15 +0200

    timer detection position continued

--- clock/js/clock.js
+++ clock/js/clock.js
@@ -4,7 +4,11 @@ var elem = document.documentElement;
 var btn = document.getElementById("FullscreenButton");
 var canv = document.getElementById("ClockCanvas");
 var ctx = canv.getContext("2d");
-var squareSize = 100;
+var lastTime = Date.now();
+var mousePosition = {x:0, y:0, speed:0, pred_x:0, pred_y:0};
+var background = new Image();
+background.src = "img/background.jpg";
+var currentTime = new Date();
 
 /* View in fullscreen */
 function openFullscreen() {
@@ -36,16 +40,9 @@ function closeFullscreen() {
 }
 
 window.onresize = function() {
-  canv.width = window.innerWidth;
-  canv.height = window.innerHeight;
-  
-  for (let i=1; i<50; i++) {
-	ctx.beginPath();
-	ctx.arc(canv.width/2, canv.height/2, canv.height/2/i, 0, 2 * Math.PI);
-	ctx.stroke();
-    }
+	canv.width = window.innerWidth;
+	canv.height = window.innerHeight;
 }
-
 
 function getRndColor() {
     var r = 255*Math.random()|0,
@@ -54,44 +51,96 @@ function getRndColor() {
     return 'rgb(' + r + ',' + g + ',' + b + ')';
 }
 
-function update(dt) {
-	if (1/dt >= 55) {
-		squareSize*=0.99;
+function isPointOnTime(x, y) {
+	let x_cutoff = 0;
+	let	y_cutoff = 0;
+	let unitSquare = 0;
+	if (canv.width*9/16 > canv.height) {
+		x_cutoff = (canv.width - canv.height*16/9) / 2;
+		unitSquare = canv.height / 9;
 	} else {
-		squareSize*=1.01;
+		y_cutoff = (canv.height - canv.width*9/16) / 2;
+		unitSquare = canv.width / 16;
 	}
+	
+	if (x<x_cutoff || x>canv.width-x_cutoff || y<y_cutoff || y>canv.height-y_cutoff) {
+		return false;
+	}
+	
+	x -= x_cutoff;
+	y -= y_cutoff;
+	
+	if (x<unitSquare || x>15*unitSquare || y<unitSquare || y>8*unitSquare) {
+		return false;
+	} else {
+		return true;
+	}
+	
+	
+}
+
+function updateMouse(canv, evt) {
+	var rect = canv.getBoundingClientRect();
+	var new_x = evt.clientX - rect.left;
+	var new_y = evt.clientY - rect.top;
+	var new_speed = Math.sqrt(Math.pow(new_x-mousePosition.x, 2) + Math.pow(new_y-mousePosition.y, 2));
+	var new_pred_x = new_x + 2 * (new_x - mousePosition.x);
+	var new_pred_y = new_y + 2 * (new_y - mousePosition.y);
+	mousePosition = {x:new_x, y:new_y, speed:new_speed, pred_x:new_pred_x, pred_y:new_pred_y};
+}
+
+function update(dt) {
 }
 
 
 function render() {
-	for (let x=0; x<canv.width/squareSize; x++)
-		for (let y=0; y<canv.height/squareSize; y++) {
-			ctx.fillStyle = getRndColor();
-			ctx.fillRect(x*squareSize, y*squareSize, squareSize, squareSize);
-		}	
+	ctx.drawImage(background, 0, 0, canv.width, canv.height);
+	
+	ctx.beginPath();
+	ctx.arc(mousePosition.pred_x, mousePosition.pred_y, mousePosition.speed, 0, 2 * Math.PI);
+	ctx.stroke();
+	
+	for (let i=0; i<100; i++) {
+		let x = Math.random() * canv.width;
+		let y = Math.random() * canv.height;
+		if (isPointOnTime(x, y)) {
+			ctx.fillStyle = "black";
+		} else {
+			ctx.fillStyle = "white"
+		}
+		ctx.beginPath();
+		ctx.arc(x, y, 5, 0, 2 * Math.PI);
+		ctx.fill();
+	}
 }
 
 function renderFPS(dt) {
 	ctx.fillStyle = "white";
-	ctx.fillRect(0, 0, 90, 60);
-	ctx.font = "20px Arial";
+	ctx.fillRect(0, 0, 40, 15);
+	ctx.font = "12px Arial";
 	ctx.fillStyle = "black";
-	ctx.fillText((1/dt).toFixed() + "fps", 10, 30);
-	ctx.fillText("size=" + (squareSize).toFixed(), 10, 50);
+	ctx.fillText((1/dt).toFixed() + " fps", 2, 12);
+	
+	
 }
 
-
-var lastTime;
 function animate() {
 	var now = Date.now();
     var dt = (now - lastTime) / 1000.0;
-
+	
+	currentTime = new Date();
+	
     update(dt);
     render();
 	renderFPS(dt);
-
-    lastTime = now;
+	
+	lastTime = now;
 	requestAnimationFrame(animate);
 }
+
+
+canv.addEventListener('mousemove', function(evt) {
+    updateMouse(canv, evt);
+})
 
 animate()
