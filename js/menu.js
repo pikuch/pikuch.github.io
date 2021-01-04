843e907a1e8a6c7e1b6be4b588db52798476e5c4
pikuch <55052790+pikuch@users.noreply.github.com>
2021-01-04 21:48:36 +0100

    added a main menu script file

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+
+var links = ["example1", "example2", "example3", "example4"]
+
+// TODO: place links on the site
+//<ul>
+//		<li><a href="./example1/index.html"><img src="./example1/example1.png" alt="example1" width="400px" height="400px"></a></li>
+//		<li><a href="./example2/index.html"><img src="./example2/example2.png" alt="example2" width="400px" height="400px"></a></li>
+//		<li><a href="./example3/index.html"><img src="./example3/example3.png" alt="example3" width="400px" height="400px"></a></li>
+//		<li><a href="./example4/index.html"><img src="./example4/example4.png" alt="example4" width="400px" height="400px"></a></li>
+//	</ul>
