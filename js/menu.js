1b0d522adc4b2366bc461a3d286d3c86e9417551
pikuch <55052790+pikuch@users.noreply.github.com>
2021-01-05 17:06:15 +0100

    Added puzzles, squares for now

--- js/menu.js
+++ js/menu.js
@@ -1,38 +1,100 @@
 
-var linkNames = ["example1", "example2", "example3", "example4"]
-var links = []
+var linkNames = ["example1", "example2", "example3", "example4"];
+var images = {};
+for (let n in linkNames) {
+	let img = document.createElement("img");
+	img.src = "./" + linkNames[n] + "/" + linkNames[n] + ".png";
+	images[linkNames[n]] = img;
+}
 
-var linkImportanceEasing = 0.1;
-var mouseX = 0;
-var mouseY = 0;
 
 var canv = document.createElement("canvas");  
 var ctx = canv.getContext("2d");
 
-function Link(name) {
-	this.name = name;
-	this.target = "./" + name + "/index.html";
-	this.importance = 0.01;
-	this.radius = 1;
-	this.x = 0;
-	this.y = 0;
-	this.fx = 0; // force vector
-	this.fy = 0;
-	this.img = document.createElement("img");
-	this.img.src = "./" + name + "/" + name + ".png";
-	
-	this.addForceFrom = function (x, y, otherImportance) {
-		relativeImportance = otherImportance / this.importance;
-		dist = 0.001 * Math.max(1, Math.hypot(this.x - x, this.y - y));
-		dir = Math.atan2(this.y - y, this.x - x);
-		this.fx += relativeImportance * Math.cos(dir) / (dist * dist);
-		this.fy += relativeImportance * Math.sin(dir) / (dist * dist);
+var mouseX = 0;
+var mouseY = 0;
+
+var puzzle;
+
+class Puzzle {
+
+	constructor(canv, linkNames) {
+		this.tilesX = 1;
+		this.tilesY = 1;
+		this.tileSize = 0;
+		this.findDivisions(linkNames.length + 1);
+		this.tiles = [];
+		this.generateTiles(linkNames);
+	}
+
+	generateTiles(linkNames) {
+		let toFill = [];
+		for (let x = 0; x < this.tilesX; x++) {
+			this.tiles.push([]);
+			for (let y = 0; y < this.tilesY; y++) {
+				let newTile = new Tile();
+				this.tiles[x].push(newTile);
+				toFill.push(newTile);
+			}
+		}
+		for (let n in linkNames) {
+			let chosen = Math.floor(Math.random() * toFill.length);
+			toFill[chosen].addLink(linkNames[n]);
+			toFill.splice(chosen, 1);
+        }
+    }
+
+	findDivisions(minElements) {
+		let margin = 0.2;
+		let width = canv.width * (1 - margin);
+		let height = canv.height * (1 - margin);
+		while (this.tilesX * this.tilesY < minElements) {
+			if (width / this.tilesX < height / this.tilesY) {
+				this.tilesY++;
+			} else {
+				this.tilesX++;
+			}
+		}
+		this.tileSize = Math.min(width / this.tilesX, height / this.tilesY);
+    }
+
+	draw() {
+		let startX = canv.width / 2 - this.tilesX / 2 * this.tileSize;
+		let startY = canv.height / 2 - this.tilesY / 2 * this.tileSize;
+		for (let x = 0; x < this.tilesX; x++) {
+			for (let y = 0; y < this.tilesY; y++) {
+				if (typeof this.tiles[x][y].name !== 'undefined') {
+
+					//ctx.save();
+					//ctx.beginPath();
+					//ctx.arc(links[n].x, links[n].y, links[n].radius, 0, 2 * Math.PI);
+					//ctx.closePath();
+					//ctx.clip();
+					//ctx.restore();
+
+					ctx.drawImage(this.tiles[x][y].img, startX + x * this.tileSize, startY + y * this.tileSize, this.tileSize, this.tileSize);
+				}
+			}
+		}
+	}
+
+}
+
+class Tile {
+	addLink(name) {
+		this.name = name;
+		this.target = "./" + name + "/index.html";
+		this.img = images[name];
 	}
 }
+
 
 window.onresize = function() {
 	canv.width = window.innerWidth;
 	canv.height = window.innerHeight;
+
+	puzzle = new Puzzle(canv, linkNames);
+	render();
 }
 
 window.onload = function () {
@@ -40,116 +102,16 @@ window.onload = function () {
 	canv.height = window.innerHeight;
 	document.body.appendChild(canv);
 	
-	for (n in linkNames) {
-		links.push(new Link(linkNames[n]));
-	}
-	for (n in links) {  // position links in a circle
-		let angle = (n / links.length) * 2 * Math.PI;
-		links[n].x = canv.width/2 + canv.width/4 * Math.cos(angle);
-		links[n].y = canv.height/2 + canv.height/4 * Math.sin(angle);
-	}
-
+	puzzle = new Puzzle(canv, linkNames);
+	render();
 }
 
-function update() {
-	// update link importance (relative size)
-	for (n in links) {
-		if (links[n].importance != 1.0) {
-			links[n].importance += (1.0 - links[n].importance) * linkImportanceEasing;
-		}
-	}
-	
-	// calculate forces
-	for (n in links) {
-		links[n].fx = 0.0;
-		links[n].fy = 0.0;
-		for (m in links) {
-			if (n != m) {
-				links[n].addForceFrom(links[m].x, links[m].y, links[m].importance);
-			}
-		}
-		links[n].addForceFrom(links[n].x, 0, 1);
-		links[n].addForceFrom(links[n].x, canv.height, 1);
-		links[n].addForceFrom(0, links[n].y, 1);
-		links[n].addForceFrom(canv.width, links[n].y, 1);
-	}
-
-	// move links according to forces
-	for (n in links) {
-		links[n].fx = Math.min(links[n].fx, 10);
-		links[n].fx = Math.max(links[n].fx, -10);
-		links[n].fy = Math.min(links[n].fy, 10);
-		links[n].fy = Math.max(links[n].fy, -10);
-		
-		links[n].x += links[n].fx;
-		links[n].y += links[n].fy;
-	}
-	
-	// if a link goes outside the canvas, bring it inside
-	for (n in links) {
-		if (links[n].x < 0) {
-			links[n].x = 1;
-		}
-		if (links[n].x > canv.width) {
-			links[n].x = canv.width - 1;
-		}
-		if (links[n].y < 0) {
-			links[n].y = 1;
-		}
-		if (links[n].y > canv.height) {
-			links[n].y = canv.height - 1;
-		}
-	}
-	
-	// update link with mouseover
-	for (n in links) {
-		if (Math.hypot(links[n].x - mouseX, links[n].y - mouseY) < links[n].radius) {
-			links[n].importance += (3 - links[n].importance);
-		}
-	}
-}
 
 function render() {
 	ctx.clearRect(0, 0, canv.width, canv.height);
-
-	for (n in links) {
-		// update image radius for drawing
-		links[n].radius = canv.width;
-		// take into account distances to other links
-		for (m in links) {
-			if (n != m) {
-				r = Math.hypot(links[n].x-links[m].x, links[n].y-links[m].y) / 2;
-				if (r < links[n].radius) {
-					links[n].radius = r;
-				}
-			}
-		}
-		// take into account distances to borders
-		if (links[n].radius > links[n].y) {
-			links[n].radius = links[n].y;
-		}
-		if (links[n].radius > canv.height - links[n].y) {
-			links[n].radius = canv.height - links[n].y;
-		}
-		if (links[n].radius > links[n].x) {
-			links[n].radius = links[n].x;
-		}
-		if (links[n].radius > canv.width - links[n].x) {
-			links[n].radius = canv.width - links[n].x;
-		}
-		
-		// draw mask
-		ctx.save();
-		ctx.beginPath();
-		ctx.arc(links[n].x, links[n].y, links[n].radius, 0, 2 * Math.PI);
-		ctx.closePath();
-		ctx.clip();
-		// draw image
-		ctx.drawImage(links[n].img, links[n].x-links[n].radius, links[n].y-links[n].radius, 2*links[n].radius, 2*links[n].radius);
-		ctx.restore();
-
-	}
+	puzzle.draw();
 }
+
 
 function updateMouse(evt) {
 	let rect = canv.getBoundingClientRect();
@@ -158,20 +120,15 @@ function updateMouse(evt) {
 }
 
 function clickMouse(evt) {
-	for (n in links) {
-		if (Math.hypot(links[n].x - mouseX, links[n].y - mouseY) < links[n].radius) {
-			location.href = links[n].target;
-		}
-	}		
+	
 }
 
-function animate() {	
-    update();
-	render();	
-	requestAnimationFrame(animate);
-}
+// function animate() {	
+	// render();	
+	// requestAnimationFrame(animate);
+// }
 
 canv.addEventListener("mousemove", function(evt) { updateMouse(evt);} );
 canv.addEventListener("mousedown", function(evt) { clickMouse(evt);} );
 
-animate();
+//animate();
