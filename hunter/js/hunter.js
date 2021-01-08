a76a365370e6cce7df43ccf5ae900b1e1534646b
pikuch <55052790+pikuch@users.noreply.github.com>
2021-01-08 15:44:02 +0100

    two rendering methods

--- hunter/js/hunter.js
+++ hunter/js/hunter.js
@@ -21,19 +21,33 @@ function render() {
 	ctx.clearRect(0, 0, canv.width, canv.height);
 	ctx.lineWidth = 1;
 	ctx.strokeStyle = "#000000";
+	ctx.fillStyle = "rgba(0, 0, 0, 0.01)";
 
-	if (mode == 0) {
+	if (mode == 0) {  // 1000 lines
 		ctx.beginPath();
-		for (let r=0; r<100; r++){
+		for (let r=0; r<10; r++){
 			for (let a=0; a<1; a+=0.01) {
-				let x = canv.width/2 + r*Math.cos(a*2*Math.PI);
-				let y = canv.height/2 + r*Math.sin(a*2*Math.PI);
-				ctx.moveTo(x, y);
-				ctx.lineTo(x+5, y+5);
+				angle = a + Math.random() * 10;
+				radius = (r + Math.random()) * 10;
+				let x0 = canv.width/2 + radius*Math.cos(angle*2*Math.PI);
+				let y0 = canv.height/2 + radius*Math.sin(angle*2*Math.PI);
+				let x = canv.width/2 + (radius+7)*Math.cos(angle*2*Math.PI);
+				let y = canv.height/2 + (radius+7)*Math.sin(angle*2*Math.PI);
+				ctx.moveTo(x0, y0);
+				ctx.lineTo(x, y);
 			}
 		}
 		ctx.closePath();
 		ctx.stroke();
+	} else if (mode == 1) {  // 1000 circles
+		for (let n=0; n<1000; n++) {
+			ctx.beginPath();
+			let x = canv.width/2 + Math.random()*400-200;
+			let y = canv.height/2 + Math.random()*400-200;
+			ctx.arc(x, y, 100, 0, 2*Math.PI);
+			ctx.closePath();
+			ctx.fill();
+		}
 	}
 	
 }
