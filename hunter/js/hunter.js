cfe2caa58b5c386db1d34e26334441598d071c87
pikuch <55052790+pikuch@users.noreply.github.com>
2021-01-09 14:59:17 +0100

    started working on the hunter

--- hunter/js/hunter.js
+++ hunter/js/hunter.js
@@ -1,9 +1,6 @@
 
 var canv = document.createElement("canvas");  
 var ctx = canv.getContext("2d");
-var img = document.createElement("img");
-img.src = "./AlphaBall.png";
-var mode = 0;
 
 window.onresize = function() {
 	resizeAndRedrawCanvas();
@@ -21,43 +18,22 @@ function resizeAndRedrawCanvas() {
 
 function render() {
 	ctx.clearRect(0, 0, canv.width, canv.height);
-	ctx.lineWidth = 1;
+	ctx.lineWidth = 10;
 	ctx.strokeStyle = "#000000";
-	ctx.fillStyle = "rgba(0, 0, 0, 0.01)";
 
-	if (mode == 0) {  // 1000 lines
-		ctx.beginPath();
-		for (let r=0; r<10; r++){
-			for (let a=0; a<1; a+=0.01) {
-				angle = a + Math.random() * 10;
-				radius = (r + Math.random()) * 10;
-				let x0 = canv.width/2 + radius*Math.cos(angle*2*Math.PI);
-				let y0 = canv.height/2 + radius*Math.sin(angle*2*Math.PI);
-				let x = canv.width/2 + (radius+7)*Math.cos(angle*2*Math.PI);
-				let y = canv.height/2 + (radius+7)*Math.sin(angle*2*Math.PI);
-				ctx.moveTo(x0, y0);
-				ctx.lineTo(x, y);
-			}
-		}
-		ctx.closePath();
-		ctx.stroke();
-	} else if (mode == 1) {  // 1000 circles
-		for (let n=0; n<1000; n++) {
-			ctx.beginPath();
-			let x = canv.width/2 + Math.random()*400-200;
-			let y = canv.height/2 + Math.random()*400-200;
-			ctx.arc(x, y, 100, 0, 2*Math.PI);
-			ctx.closePath();
-			ctx.fill();
-		}
-	} else {  // 1000 images with alpha
-		for (let n=0; n<1000; n++) {
-			let x = 0|(canv.width/2 + Math.random()*400-200);
-			let y = 0|(canv.height/2 + Math.random()*400-200);
-			ctx.drawImage(img, x, y);
-		}
+	ctx.beginPath();
+	for (let n=0; n<200; n++) {
+		let angle = Math.random()*2*Math.PI;
+		let r = Math.random() * 100;
+		let x0 = canv.width/2 + r * Math.cos(angle);
+		let y0 = canv.height/2 + r * Math.sin(angle);
+		let x = canv.width/2 - r * Math.cos(angle);
+		let y = canv.height/2 - r * Math.sin(angle);
+		ctx.moveTo(x0, y0);
+		ctx.lineTo(x, y);
 	}
-	
+	ctx.closePath();
+	ctx.stroke();	
 }
 
 function clickMouse(evt) {
